0f6b384707fe03c647102cd97c13281bbb9ac1e2
Daniel Lehmann <leh@l3h.de>
2011-03-30 18:49:30 +0200

    Admins can edit the notifo settings of users.

--- lib/redmine_notifo/hooks/view_my_account_hook.rb
+++ lib/redmine_notifo/hooks/view_my_account_hook.rb
@@ -2,6 +2,7 @@ module RedmineNotifo
   module Hooks
     class ViewMyAccountHook < Redmine::Hook::ViewListener
       render_on(:view_my_account, :partial => 'notifo_settings/settings', :layout => false)
+      render_on(:view_users_form, :partial => 'notifo_settings/settings', :layout => false)
     end
   end
 end
